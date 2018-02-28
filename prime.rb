363e56ddf97fe52552f9379f0d0f50dc5fd8908b
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 09:39:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 # Add  code here!
 def prime?(num)
   for counter in 2..num-1 do
-    if num % counter == 0
+    if num <= 1
+      return false
+    elsif num % counter == 0
       return false
     end
   end
